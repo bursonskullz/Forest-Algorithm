599b8ba8e0df770128888b94b805e6820ec42994
bursonskullz <159820187+bursonskullz@users.noreply.github.com>
2024-02-19 07:14:53 -0800

    Update makedata.py

--- makedata.py
+++ makedata.py
@@ -3,10 +3,10 @@ import math as mt
 
 data = []
 def makedomain():
-	domain = np.linspace(0, 2*mt.pi, 500)
+	domain = np.linspace(-4, 4, 200)
 	for t in domain:
-		x = 2.14* mt.cos(t)
-		y = 18.1877* mt.sin(t)
+		x = mt.pi+ mt.cos(t)
+		y = mt.pi * mt.tan(t)+mt.sin(t)
 		point = [x,y]
 		data.append(point)
 makedomain()
@@ -14,3 +14,4 @@ makedomain()
 with open("domain.txt", "w") as txt_file:
     for line in data:
         txt_file.write('['+ str(line[0]) + "," + str(line[1]) +']' + '\n')
+txt_file.close()
